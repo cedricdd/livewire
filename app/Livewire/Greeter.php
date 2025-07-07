cd06ba548e63a9fe23618c357e4f058f3bce2b0b
Cedric Den Drijver <c-d-d@hotmail.fr>
2025-07-07 16:29:30 +0200

    basic validation

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -3,16 +3,42 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Validation\Rule;
+
+const GREETINGS = [
+    'Hello',
+    'Hi',
+    'Greetings',
+    'Welcome',
+    'Salutations',
+];
 
 class Greeter extends Component
 {
     public string $name = '';
     public string $greeting = '';
+    public string $message = '';
+    public string $instant = '';
 
-    public function changeName(): void{}
+    public function changeGreeting(){
+        $this->reset('message');
+
+        $this->validate();
+
+        $this->message = $this->greeting . ' ' . $this->name . '!';
+    }
+
+    public function rules() {
+        return [
+            'greeting' => ['required', 'string', Rule::in(GREETINGS)],
+            'name' => 'required|string|min:2|max:50',
+        ];
+    }
 
     public function render()
     {
-        return view('livewire.greeter');
+        return view('livewire.greeter', [
+            'greetings' => GREETINGS,
+        ]);
     }
 }
--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,21 +1,41 @@
 <div>
-    <div class="mt-2">
-        <select class="p-4 rounded-md bg-gray-700 w-full" wire:model.fill.change="greeting">
-            <option value="Hello">Hello</option>
-            <option value="Hi">Hi</option>
-            <option value="Hey">Hey</option>
-            <option value="Howdy">Howdy</option>
-        </select>
+    <div>
+        <input type="text" class="rounded-md px-4 py-2 border w-full mb-4 bg-white/5 placeholder-white"
+            wire:model.live.debounce.500ms="instant" placeholder="Type Here - Instant Update" />
+        <p>
+            {{ $instant }}
+        </p>
     </div>
-    <div class="mt-2">
-        <input type="text" class="rounded-md px-4 py-2 border w-full mb-4 bg-white/5" wire:model.live.debounce.500ms="name" placeholder="Enter Name" />
-    </div>
+    <hr class="my-8">
 
-    @if($name !== '')
-        <div class="mt-4">
-            <p class="text-sm text-gray-400">
-                {{ $greeting }}, {{ $name }}!
-            </p>
+    <form action="" wire:submit="changeGreeting">
+        <div class="mt-2">
+            <select
+                class="border rounded-lg block w-full p-2.5 bg-black/60 border-gray-600 placeholder-gray-400 focus:border-blue-500"
+                wire:model.fill.defer="greeting">
+                @foreach ($greetings as $greeting)
+                    <option value="{{ $greeting }}">{{ $greeting }}</option>
+                @endforeach
+            </select>
+        </div>
+        @error('greeting')
+            <div class="text-red-500 text-sm my-2">{{ $message }}</div>
+        @enderror
+        <div class="mt-2">
+            <input type="text" class="rounded-md px-4 py-2 border w-full bg-white/5 placeholder-white"
+                wire:model.defer="name" placeholder="Enter Name" />
+        </div>
+        @error('name')
+            <div class="text-red-500 text-sm my-2">{{ $message }}</div>
+        @enderror
+        <button
+            class="mt-2 border border-blue-600 rounded-md py-2 px-4 bg-blue-500 hover:bg-blue-700 font-bold cursor-pointer"
+            type="submit">Submit</button>
+    </form>
+
+    @if (!empty($message))
+        <div class="mt-4 text-lg">
+            {{ $message }}
         </div>
     @endif
 </div>
